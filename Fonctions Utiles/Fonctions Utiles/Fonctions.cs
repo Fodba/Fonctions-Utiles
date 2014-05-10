0e9777d8b1b9418dc4e29fb04930a59ab5e82aa6
Fef <ffodba@gmail.com>
2014-05-10 21:45:42 +0200

    Fonction "Poursuivre"
    Signed-off-by: Fef <ffodba@gmail.com>

---
+++ Fonctions Utiles/Fonctions Utiles/Fonctions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fonctions_Utiles
+{
+    public static class Fonctions
+    {
+        /// <summary>
+        /// fonctions permettant à l'utilisateur de continuer ou arréter le programme.
+        /// </summary>
+        /// <param name="continuer"></param>
+        public static void Poursuivre(out bool continuer)
+        {
+            string reponse = "";
+            continuer = true;
+            while (reponse != "n" && reponse != "o")
+            {
+                Console.WriteLine("Voulez-vous recommencer?\no:Oui\tn:non");
+                reponse = Console.ReadLine().ToLower();
+                if (reponse == "n")
+                {
+                    continuer = false;
+                }
+                else if (reponse != "n" && reponse != "o")
+                {
+                    Console.WriteLine("Je n'ai pas saisie votre réponse!");
+                }
+            }
+        }
+    }
+}
